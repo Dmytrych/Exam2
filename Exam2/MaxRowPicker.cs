e9abb0d66617e509c8e0f7cdae2122bcf916ce2b
Dmytro <karambolrul@gmail.com>
2020-06-06 13:59:33 +0300

    Add project files.

---
+++ Exam2/MaxRowPicker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2
+{
+    class MaxRowPicker
+    {
+        public string InputPath { get; private set; }
+        public MaxRowPicker(string inputPath)
+        {
+            InputPath = inputPath;
+        }
+        public void WriteMaxSumRow()
+        {
+            StreamReader reader = new StreamReader(InputPath);
+            int currentRow = 1,
+                maxRow = -1,
+                maxValue = -1,
+                currentValue;
+            while(reader.EndOfStream == false)
+            {
+                currentValue = CountRow(reader.ReadLine());
+                if(currentValue > maxValue)
+                {
+                    maxValue = currentValue;
+                    maxRow = currentRow;
+                }
+                currentRow++;
+            }
+            Console.WriteLine(maxRow + ": " + maxValue);
+        }
+        private int CountRow(string row)
+        {
+            row += " ";
+            string num = "";
+            int sum = 0;
+            bool numFormationEnded = true;
+            foreach(char c in row)
+            {
+                if(c != ' ')
+                {
+                    num += c;
+                    numFormationEnded = false;
+                }
+                else if(numFormationEnded == false)
+                {
+                    sum += int.Parse(num);
+                    num = "";
+                    numFormationEnded = true;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Exam2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = new StreamWriter("input.txt");
+            writer.WriteLine("123   2333 3 5  23");
+            writer.WriteLine("123   23 3 5  23");
+            writer.Close();
+            MaxRowPicker picker = new MaxRowPicker("input.txt");
+            picker.WriteMaxSumRow();
+            Console.ReadKey();
+        }
+    }
+}
